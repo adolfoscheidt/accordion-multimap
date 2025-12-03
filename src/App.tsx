b1fa511b09d4d5e0a90c3e1eade5e80f9717767a
Adolfo Scheidt <scheidtadolfo@gmail.com>
2025-12-03 19:43:10 -0300

    WIP tonejs demo

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,34 @@
+import { useEffect, useRef, useState } from "react";
+import { Sampler } from "tone";
+import A1 from "./assets/piano-mp3/A1.mp3";
+
 function App() {
+  const sampler = useRef<Sampler>(null);
+  const [isLoaded, setIsLoaded] = useState(false);
+
+  const onSamplerLoaded = () => {
+    setIsLoaded(true);
+  };
+
+  useEffect(() => {
+    sampler.current = new Sampler({ A1 }, onSamplerLoaded).toDestination();
+  }, []);
+
+  const handleMouseDown = () => sampler.current?.triggerAttack("A1");
+  const handleMouseUp = () => sampler.current?.triggerRelease("A1");
+
+  return (
+    <div>
+      <button
+        disabled={!isLoaded}
+        onMouseDown={handleMouseDown}
+        onMouseUp={handleMouseUp}
+      >
+        Press to play
+      </button>
+    </div>
+  );
+
   return null;
 }
 
